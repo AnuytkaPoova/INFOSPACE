107caf4b3cd687dad05998d6d4a674d0393e9d72
APopova <a_ches@mail.ru>
2021-09-27 17:31:07 +0300

    HW_3_v3

--- app/src/main/java/com/a_ches/infospace/ui/picture/mars/PODServerResponseDataOfMars.kt
+++ app/src/main/java/com/a_ches/infospace/ui/picture/mars/PODServerResponseDataOfMars.kt
@@ -4,6 +4,31 @@ import com.google.gson.annotations.SerializedName
 
 data class PODServerResponseDataOfMars (
 
+        @field:SerializedName("copyright") val copyright: String?,
+        @field:SerializedName("date") val date: String?,
+        @field:SerializedName("explanation") val explanation: String?,
+        @field:SerializedName("media_type") val mediaType: String?,
+        @field:SerializedName("title") val title: String?,
+        @field:SerializedName("url") val url: String?,
+        @field:SerializedName("hdurl") val hdurl: String?
+)
+
+/*
+ @SerializedName("camera")
+    val camera: Camera,
+    @SerializedName("earth_date")
+    val earthDate: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("img_src")
+    val imgSrc: String,
+    @SerializedName("rover")
+    val rover: Rover,
+    @SerializedName("sol")
+    val sol: Int
+ */
+
+/*
     @field:SerializedName("copyright") val copyright: String?,
     @field:SerializedName("date") val date: String?,
     @field:SerializedName("explanation") val explanation: String?,
@@ -11,4 +36,15 @@ data class PODServerResponseDataOfMars (
     @field:SerializedName("title") val title: String?,
     @field:SerializedName("url") val url: String?,
     @field:SerializedName("hdurl") val hdurl: String?
-)
+ */
+
+/*
+   @field:SerializedName("camera") val camera: String?,
+    @field:SerializedName("earth_date") val earth_date: String?,
+    @field:SerializedName("id") val id: String?,
+    @field:SerializedName("img_src") val img_src: String?,
+    @field:SerializedName("rover") val rover: String?,
+    @field:SerializedName("sol") val sol: String?,
+    @field:SerializedName("url") val url: String?,
+    @field:SerializedName("hdurl") val hdurl: String?
+ */
