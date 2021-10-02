5bedef1641a812aec832a787524210fdc5a0e0f8
APopova <a_ches@mail.ru>
2021-10-02 16:07:52 +0300

    HW_5_v1

--- app/src/main/java/com/a_ches/infospace/ui/picture/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/com/a_ches/infospace/ui/picture/BottomNavigationDrawerFragment.kt
@@ -1,20 +1,23 @@
 package com.a_ches.infospace.ui.picture
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.a_ches.infospace.R
+import com.a_ches.infospace.ui.animations.AnimationsActivity
+import com.a_ches.infospace.ui.animations.AnimationsActivityBonus
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.bottom_navigation_layout.*
 
 class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.bottom_navigation_layout, container, false)
     }
@@ -24,10 +27,24 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
 
         navigation_view.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.navigation_one -> Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
-                R.id.navigation_two -> Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
-
+                R.id.navigation_one -> activity?.let {
+                    startActivity(
+                        Intent(
+                            it,
+                            AnimationsActivity::class.java
+                        )
+                    )
+                }
+                R.id.navigation_two -> activity?.let {
+                    startActivity(
+                        Intent(
+                            it,
+                            AnimationsActivityBonus::class.java
+                        )
+                    )
+                }
             }
+            dismiss()
             true
         }
     }
